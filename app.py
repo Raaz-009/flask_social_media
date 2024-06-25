95f29da617db7ce52e4042f0c7dfd42d31c395ab
Raaz-009 <thameemraz829@gmail.com>
2024-06-25 21:18:51 +0530

    fixed most of things

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, render_template, redirect, url_for, request,abort
+from flask import Flask, flash, render_template, redirect, url_for, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from flask_wtf import FlaskForm
@@ -9,11 +9,14 @@ from flask_wtf.file import FileAllowed
 from werkzeug.utils import secure_filename
 import uuid
 import os
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'secretkey'
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -27,7 +30,16 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-#Class for user details and login
+class Follow(db.Model):
+    __tablename__ = 'follow'
+    follower_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    followed_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    follower = db.relationship('User', foreign_keys=[follower_id], backref=db.backref('followed_by', lazy='dynamic'))
+    followed = db.relationship('User', foreign_keys=[followed_id], backref=db.backref('follows', lazy='dynamic'))
+
+# Class for user details and login
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
@@ -35,15 +47,22 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(80), nullable=False)
     bio = db.Column(db.Text, nullable=True)
     profile_picture = db.Column(db.String(255), nullable=True)
-    posts = db.relationship('Post', backref='author', lazy=True)   #for posts 
+    posts = db.relationship('Post', backref='author', lazy=True)  # for posts 
+    following = db.relationship(
+        'User', secondary='follow',
+        primaryjoin=(Follow.follower_id == id),
+        secondaryjoin=(Follow.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+    )
 
 
-#class for user posts
+# Class for user posts
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image_filename = db.Column(db.String(255), nullable=False)  # Ensure this line exists
     caption = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.now)
 
 with app.app_context():
     db.create_all()
@@ -76,7 +95,7 @@ class LoginForm(FlaskForm):
             raise ValidationError("Invalid username, email, or password")
 
 
-#form for deleting and adding posts
+# Form for deleting and adding posts
 
 class PostForm(FlaskForm):
     photo = FileField('Photo', validators=[InputRequired(), FileAllowed(['jpg', 'jpeg', 'png'], 'Images only!')])
@@ -85,8 +104,6 @@ class PostForm(FlaskForm):
 
 class DeletePostForm(FlaskForm):
     submit = SubmitField('Delete')
-
-
 
 
 UPLOAD_FOLDER = r'static/images'
@@ -98,9 +115,6 @@ class ProfileSetupForm(FlaskForm):
     bio = TextAreaField('Bio', render_kw={"placeholder": "Tell us about yourself"})
     profile_picture = FileField('Profile Picture', validators=[FileAllowed(ALLOWED_EXTENSIONS, 'Only images allowed')])
     submit = SubmitField('Save Changes')
-
-# with app.app_context():
-#     db.create_all()
 
 @app.route('/')
 def home():
@@ -137,7 +151,13 @@ def dashboard():
 
     posts = Post.query.filter_by(user_id=current_user.id).all()
     delete_form = DeletePostForm()
-    return render_template('dashboard.html', form=form, posts=posts, delete_form=delete_form)
+
+    followers_count = current_user.followers.count()
+    following_count = current_user.following.count()
+
+    return render_template('dashboard.html', form=form, posts=posts, delete_form=delete_form, 
+                           followers_count=followers_count, following_count=following_count)
+
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -152,7 +172,7 @@ def register_user():
     return render_template('register.html', form=form)
 
 
-#post photo route
+# Post photo route
 @app.route('/post_photo', methods=['GET', 'POST'])
 @login_required
 def post_photo():
@@ -186,6 +206,74 @@ def delete_post(post_id):
     db.session.commit()
     flash('Post deleted successfully!', 'success')
     return redirect(url_for('dashboard'))
+
+@app.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        search_term = request.form.get('search_term')
+        users = User.query.filter(User.username.like(f'%{search_term}%')).all()
+        return render_template('search.html', users=users)
+    return render_template('search.html')
+
+
+@app.route('/follow/<int:user_id>', methods=['POST'])
+@login_required
+def follow(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        flash('User not found.', 'danger')
+        return redirect(url_for('search'))
+    if user == current_user:
+        flash('You cannot follow yourself!', 'danger')
+        return redirect(url_for('user_profile', username=user.username))
+    current_user.following.append(user)
+    db.session.commit()
+    flash(f'You are now following {user.username}!', 'success')
+    return redirect(url_for('user_profile', username=user.username))
+
+@app.route('/unfollow/<int:user_id>', methods=['POST'])
+@login_required
+def unfollow(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        flash('User not found.', 'danger')
+        return redirect(url_for('search'))
+    if user == current_user:
+        flash('You cannot unfollow yourself!', 'danger')
+        return redirect(url_for('user_profile', username=user.username))
+    current_user.following.remove(user)
+    db.session.commit()
+    flash(f'You have unfollowed {user.username}.', 'success')
+    return redirect(url_for('user_profile', username=user.username))
+
+@app.route('/user/<username>')
+@login_required
+def user_profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(user_id=user.id).all()
+    followers_count = user.followers.count()
+    following_count = user.following.count()
+    return render_template('user_dashboard.html', user=user, posts=posts, followers_count=followers_count, following_count=following_count)
+
+
+@app.route('/feed')
+@login_required
+def feed():
+    followed_users = [user.id for user in current_user.following]
+    posts = Post.query.filter(Post.user_id.in_(followed_users)).order_by(Post.timestamp.desc()).all()
+    return render_template('feed.html', posts=posts)
+
+
+# from flask import jsonify
+
+# @app.route('/search_ajax', methods=['POST'])
+# @login_required
+# def search_ajax():
+#     search_term = request.form.get('search_term')
+#     users = User.query.filter(User.username.ilike(f'%{search_term}%')).all()
+#     return render_template('search_results.html', users=users)
+
 
 @app.route('/logout')
 @login_required
